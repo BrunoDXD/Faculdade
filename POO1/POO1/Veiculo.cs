3e86a39c75a5db8c5cf6a92cc8408b732cdaf075
BrunoDXD <146298531+BrunoDXD@users.noreply.github.com>
2023-09-27 21:15:22 -0300

    Add files via upload
    POO aula 1

---
+++ POO1/POO1/Veiculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO1
+{
+    internal class Veiculo
+    {
+        //declaração dos atributos
+        string modelo, marca, chassis, cor;
+        int ano;
+        double motor;
+
+        //métodos
+        public void receber(string mod, string mar, string ch, string c, int a, double mo)
+        {
+            this.modelo = mod;
+            this.marca = mar;
+            this.chassis = ch;
+            this.cor = c;
+            this.motor = mo;
+            this.ano = a;
+
+        }
+
+        public string retorna()
+        {
+            string texto = "Marca = " + this.marca + "\tModelo = " + this.modelo + "\tChassis = "
+                + this.chassis + "\nCor = " + this.cor + "\tMotor = " + this.motor + "\tAno = " + this.ano;
+            return texto;
+        }
+    }
+}
