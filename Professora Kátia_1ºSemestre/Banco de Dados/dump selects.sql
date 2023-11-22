9257b338cdeb4eea55734ef1ab70861196564392
BrunoDXD <146298531+BrunoDXD@users.noreply.github.com>
2023-11-22 17:19:37 -0300

    Delete Professora Kátia_1ºSemestre/Banco de Dados directory

--- Professora Kátia_1ºSemestre/Banco de Dados/dump selects.sql
+++
@@ -1,9 +0,0 @@
-describe tb01_militares;
-describe tb02_patentes;
-describe tb03_funcao;
-
-select * From tb01_militares;
-select * From tb01_militares where tb01_rm=3;
-select * From tb01_militares where tb01_soldo>=10000;
-select count(*) as quantidade From tb01_militares;
-select sum(tb01_soldo) as soma From tb01_militares;
