45171f52e7da13ebb5f8c679711aa8ba73ad692b
BrunoDXD <146298531+BrunoDXD@users.noreply.github.com>
2023-11-22 16:55:08 -0300

    Add files via upload
    2º Semestre

---
+++ Aula 1 - Herança/Aula9/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula9
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+
+        public Pessoa(string n, string e, string t) {
+            Nome = n;
+            Endereco = e;
+            Telefone = t;
+        }
+    }
+}
