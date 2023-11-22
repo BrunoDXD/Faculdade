2158caf23ba9970969d77b67b107747109404a31
BrunoDXD <146298531+BrunoDXD@users.noreply.github.com>
2023-11-22 17:58:31 -0300

    Add files via upload

---
+++ Professora Kátia_2ºSemestre/1ºBimestre/POO7.0(Propriets)/POO7.0(Propriets)/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO7._0_Propriets_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Produto prod = new Produto();
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            prod.Nome = txtNome.Text;
+            prod.Preco = Convert.ToDouble(txtPreco.Text);
+            MessageBox.Show("Nome: "+prod.Nome + "\nPreço: " + prod.Preco.ToString("C") +
+                "\nValor a Pagar: " +
+                (prod.Preco * Convert.ToInt32(txtQuantidade.Text)).ToString("C"));
+        }
+    }
+}
---
+++ Professora Kátia_2ºSemestre/1ºBimestre/POO8.0(List)/POO8.0(List)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO8._0_List_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> Lista = new List<Produto>();
+            Lista.Add(new Produto(1, "Óleo", 8.8));
+            Lista.Add(new Produto(2, "Arroz", 25.8));
+            Lista.Insert(4, new Produto(3, "Feijão", 8.9));
+            Produto p = new Produto(0, "", 0);
+
+            Console.WriteLine("Informe o Codigo do Produto");
+            p.Codigo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o Descrição do Produto");
+            p.Descricao = Console.ReadLine();
+
+            Console.WriteLine("Informe o Preço do Produto");
+            p.Preco = Convert.ToDouble(Console.ReadLine());
+
+            Lista.Insert(0, p);
+            Lista.Reverse();
+
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                Console.WriteLine(Lista[i].Codigo+" - " + Lista[i].Descricao
+                    +" - " + Lista[i].Preco.ToString("C"));
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+            //List<string> nome = new List<string>();
+            //List<int> numero = new List<int>();
+            //nome.Add("Joana");
+            //nome.Add("Frederico");           //Adiciona nome
+            //nome.Insert(0, "Mariana");       //Adiciona nome e posicao
+            //nome.Remove("Joana");            //remove pelo nome
+            //nome.RemoveAt(1);               //remove pela posicao
+
+        }
+    }
+}
